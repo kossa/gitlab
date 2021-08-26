1f131d4ee9db38a1c00f086ea19a98c33089c661
Kouceyla <hadjikouceyla@gmail.com>
2021-08-26 11:26:02 +0100

    Add copyright on index.php

--- index.php
+++ index.php
@@ -7,3 +7,7 @@ echo "lorem";
 <footer>
     <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Magnam illo cupiditate eum commodi quos autem, esse, natus, voluptates odio inventore quae beatae est ipsa quo doloribus sint fugiat! Necessitatibus, enim.</p>
 </footer>
+
+<div id="copyright">
+    <p>Copyright &copy; 2021</p>
+</div>
