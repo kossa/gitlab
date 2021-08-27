b4c805d71e962c3ba03cd567c0a8c0847b501f91
Kouceyla <hadjikouceyla@gmail.com>
2021-08-27 06:36:59 +0000

    feat: Adding footer in contact

--- contact.php
+++ contact.php
@@ -1,2 +1,6 @@
 <h1>contact</h1>
 <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eius autem impedit aperiam at? Nulla, porro amet facilis ipsam, aut inventore cupiditate omnis tenetur, cum quasi dicta ipsa debitis voluptatem. Eveniet!</p>
+
+<footer>
+    Just a footer
+</footer>
